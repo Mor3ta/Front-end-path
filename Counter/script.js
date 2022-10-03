a38f2959b0add066d46526aa8cc7826ac1a0dcb0
mor3ta <moreta.concepcion@gmail.com>
2022-10-03 09:24:33 -0400

    add function save

--- Counter/script.js
+++ Counter/script.js
@@ -1,6 +1,15 @@
 let counter = 0
+let previous_entries = []
 
-let increment  =()=>{
+let increment =()=>{
     counter = counter +1;
+    previous_entries.push(counter)
     document.getElementById('counter-value').innerHTML = counter;
+}
+
+let save =()=>{
+    previous_entries.forEach(number => {
+      document.getElementById('previous-values').innerHTML = `Previous entries: ${number}`; 
+    });
+    document.getElementById('counter-value').innerHTML = counter=0;
 }
